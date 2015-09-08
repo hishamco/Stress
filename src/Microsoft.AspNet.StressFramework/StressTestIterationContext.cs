85d9d9e2bc5e154124f2021dcfe01f26059fec9a
Andrew Stanton-Nurse <andrew@andrewnurse.net>
2015-09-08 12:40:56 -0700

    wip

--- src/Microsoft.AspNet.StressFramework/StressTestIterationContext.cs
+++ src/Microsoft.AspNet.StressFramework/StressTestIterationContext.cs
@@ -3,6 +3,8 @@ namespace Microsoft.AspNet.StressFramework
 	public class StressTestIterationContext
 	{
 		private List<DataPoint> _recordings = new List<DataPoint>();
+		private List<ICollector> _collectors = new List<ICollector>();
+		private Stopwatch _iterationTime = new Stopwatch();
 		
 		/// <summary>
 		/// Record a data point
@@ -11,6 +13,26 @@ namespace Microsoft.AspNet.StressFramework
 		{
 			var dataPoint = DataPoint.Create(data);
 			_recordings.Add(dataPoint);
+		}
+		
+		public void BeginIteration()
+		{
+			foreach(var collector in _collectors)
+			{
+				_collectors.BeginIteration(this);
+			}
+			
+			_iterationTime.Start();
+		}
+		
+		public void EndIteration()
+		{
+			_iterationTime.Stop();
+			
+			foreach(var collector in _collectors)
+			{
+				_collectors.EndIteration(this);
+			}
 		}
 	}
 }
